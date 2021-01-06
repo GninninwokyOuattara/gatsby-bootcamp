a7ea8cc977d9e30ff61032b028ae7a7a1306f57e
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-01-06 16:45:01 +0000

    sharing components challenge

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Footer from "../components/footer"
+import Header from "../components/header"
 
 const AboutPage = () => {
   return (
     <div>
+      <Header />
       <h1>About page</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quae
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Footer from "../components/footer"
+import Header from "../components/header"
 
 const ContactPage = () => {
   return (
     <div>
+      <Header />
       <h1>Need to contact me ?</h1>
       <Link to="https://twitter.com/IG_Ouatt">Here you go !</Link>
       <Footer />
