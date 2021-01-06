838fa8d64d20bbacf4b1a9cb9a1304cd9c8f7dda
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-01-06 13:01:39 +0000

    gatsby Link compared to html anchor tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
-
-// export default function Home() {
-//   return <div>Hello Gninninwoky!</div>
-// }
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello</h1>
       <h2>I'm Gninninwoky Ouattara, Learning Gatsby JS right now</h2>
+      <p>
+        Need a developper ? <a href="/contact">Contact ME!</a>
+      </p>
+      <p>
+        Need a developper ? <Link to="/contact">Contact ME!</Link>
+      </p>
     </div>
   )
 }
