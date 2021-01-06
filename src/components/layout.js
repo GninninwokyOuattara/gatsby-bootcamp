f5033a5073766999d921ca8e30365751f8c1ffc6
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-01-06 21:36:08 +0000

    more gatsby project styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+// import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      <div>{props.children}</div>
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        <div>{props.children}</div>
+      </div>
       <Footer />
     </div>
   )
