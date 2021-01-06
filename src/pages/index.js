1448f9b8c4e4078a3080b88e3f817369aa973bb5
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-01-06 12:32:00 +0000

    changed default gatsby index.js code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+// export default function Home() {
+//   return <div>Hello Gninninwoky!</div>
+// }
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I'm Gninninwoky Ouattara, Learning Gatsby JS right now</h2>
+    </div>
+  )
 }
+
+export default IndexPage
