d9ce07ef951c1cbd3ddbb29dc3cdfdbd82d12f1a
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-01-09 21:50:12 +0000

    markdown post and change in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,14 @@ module.exports = {
     title: "Full-stack Bootcamp",
     author: "Idriss G Ouattara",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatbsy Bootcamp"
+date: "2020-01-07"
+---
+
+I just start a new bootcamp
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQM
+3. React
