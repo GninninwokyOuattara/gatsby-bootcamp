34b7b55abd96d6898bc1c588b8cbee53c4a18599
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-02-01 21:51:26 +0000

    commit

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 
-export const quer = graphql`
+export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
