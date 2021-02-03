83506956063c6aec8bbab06d9426174b4eb1ee27
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-02-03 14:47:01 +0000

    styling components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import Footer from "../components/footer"
 import Header from "../components/header"
 import Layout from "../components/layout"
 import { graphql, useStaticQuery, Link } from "gatsby"
+import blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -27,13 +28,12 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      {/* <p>Posts will show up here later on.</p> */}
 
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((item, idx) => {
           const { node } = item
           return (
-            <li key={idx}>
+            <li key={idx} className={blogStyles.post}>
               <Link to={`./${node.fields.slug}`}>
                 <h2>{node.frontmatter.title}</h2>
                 <p>{node.frontmatter.date}</p>
