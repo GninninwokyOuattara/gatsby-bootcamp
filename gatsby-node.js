e2401381380969a4a301bdc9ffb6ff201ccb64fd
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-02-03 22:03:26 +0000

    update, remove title and publishedDate in query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,75 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
+//No longer useful as we're no longer fetching slug from local markdown
+//Switching to contentful.
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
+//   if (node.internal.type === "MarkdownRemark") {
+//     const slug = path.basename(node.fileAbsolutePath, ".md")
+//     createNodeField({
+//       node,
+//       name: "slug",
+//       value: slug,
+//     })
+//   }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
-  //retrieve the createPage action from actions
   const { createPage } = actions
-  const blogTemplate = path.resolve("./src/templates/blog.js") //Path to our blog template
-
-  //awaiting graphql data response
+  const blogTemplate = path.resolve("./src/templates/blog.js")
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `)
 
-  res.data.allMarkdownRemark.edges.forEach(edge => {
-    //create a new page for each edge
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
-      component: blogTemplate, // the component is our blogTemplate
-      path: `/blog/${edge.node.fields.slug}`, //where should the component be located
+      component: blogTemplate,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        //context is an object containing stuff we can pass down to that template
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
 }
+
+// module.exports.createPages = async ({ graphql, actions }) => {
+//   //retrieve the createPage action from actions
+//   const { createPage } = actions
+//   const blogTemplate = path.resolve("./src/templates/blog.js") //Path to our blog template
+
+//   //awaiting graphql data response
+//   const res = await graphql(`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   res.data.allMarkdownRemark.edges.forEach(edge => {
+//     //create a new page for each edge
+//     createPage({
+//       component: blogTemplate, // the component is our blogTemplate
+//       path: `/blog/${edge.node.fields.slug}`, //where should the component be located
+//       context: {
+//         //context is an object containing stuff we can pass down to that template
+//         slug: edge.node.fields.slug,
+//       },
+//     })
+//   })
+// }
