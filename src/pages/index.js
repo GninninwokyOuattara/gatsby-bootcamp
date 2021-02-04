30e2177b1abdc849897dc097003c0286df9c1dfc
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-02-04 22:56:05 +0000

    remove unnecessary line

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,6 @@ const IndexPage = () => {
       <Layout>
         <h1>Hello</h1>
         <h2>I'm Gninninwoky Ouattara, Learning Gatsby JS right now</h2>
-        <p>
-          Need a developper ? <a href="/contact">Contact ME!</a>
-        </p>
-        <p>
-          Need a developper ? <Link to="/contact">Contact ME!</Link>
-        </p>
       </Layout>
     </React.Fragment>
   )
